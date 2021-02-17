bd1f43835c11ddc7023814d28eabb0bef5a97de5
DevAlek <78972830+DevAlek@users.noreply.github.com>
2021-02-17 15:23:39 -0300

    Add files via upload

---
+++ hasher.py
@@ -0,0 +1,19 @@
+import hashlib
+import os
+try:import pyperclip
+except:
+    try:os.system('pip3 install pyperclip')
+    except:
+        try:os.system('pip install pyperclip')
+        except:print('automatic pip failed. try install pyperclip manualy'); exit()
+while True:
+    senha=str(input('> ')).encode('utf-8')
+    sha244=hashlib.sha224(senha).hexdigest().encode('utf-8')
+    sha256=hashlib.sha256(sha244).hexdigest().encode('utf-8')
+    sha3_512=hashlib.sha3_512(sha256).hexdigest().encode('utf-8')
+    sha1=hashlib.sha1(sha3_512).hexdigest().encode('utf-8')
+    sha3_384=hashlib.sha3_384(sha1).hexdigest()
+    result=sha3_384
+    pyperclip.copy(result)
+    print('hash moved to clipboard')
+    print(result)
