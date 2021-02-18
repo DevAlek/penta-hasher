2da5859030a3b0360b52beab89e1b1bac7fdaf74
DevAlek <78972830+DevAlek@users.noreply.github.com>
2021-02-18 08:17:20 -0300

    Update hasher.py

--- hasher.py
+++ hasher.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
+from ez import *
 import hashlib
 import os
-try:import pyperclip
-except:
-    try:os.system('pip3 install pyperclip')
-    except:
-        try:os.system('pip install pyperclip')
-        except:print('automatic pip failed. try install pyperclip manualy'); exit()
-senha=''
+assure(('pyperclip','keyboard'), True)
 while True:
-    try:senha=str(input('> ')).encode('utf-8')
-    except:pass
+    if keyboard.is_pressed('escape'): exit()
+    senha=str(input('> ')).encode('utf-8')
     sha244=hashlib.sha224(senha).hexdigest().encode('utf-8')
     sha256=hashlib.sha256(sha244).hexdigest().encode('utf-8')
     sha3_512=hashlib.sha3_512(sha256).hexdigest().encode('utf-8')
