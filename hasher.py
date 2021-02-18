71de660e1a90ca80035fcab0bc66a7290fb73ef8
DevAlek <78972830+DevAlek@users.noreply.github.com>
2021-02-18 12:14:21 -0300

    Update hasher.py

--- hasher.py
+++ hasher.py
@@ -9,7 +9,7 @@ while True:
     if senha.decode('utf-8')=='!':exit()
     hashs=('sha224', 'sha256', 'sha3_512', 'sha1', 'sha3_384')
     for i in hashs:
-        senha=eval(f"hashlib.{hashs}({senha})").hexdigest().encode('utf-8')
+        senha=eval(f"hashlib.{i}({senha})").hexdigest().encode('utf-8')
     senha=senha.decode('utf-8')
     try:pyperclip.copy(senha);print('hash moved to clipboard')
     except:print('failed to copy your hash automatically')
