41374cc7fd05bacd9ac9afb7d955881da1654da7
DevAlek <78972830+DevAlek@users.noreply.github.com>
2021-02-18 15:32:11 -0300

    Update hasher.py

--- hasher.py
+++ hasher.py
@@ -3,19 +3,41 @@ from random import randint as random
 from ez import *
 import hashlib
 import os
-assure(('pyperclip'), True, True)
+assure(('pyperclip', 'click'), True, True)
 import pyperclip
-senha=''
-while True:
-    try:
-        senha=str(input('> ')).encode('utf-8')
-        if senha.decode('utf-8')=='':new=random(100000, 999999);print(new);senha=str(new).encode('utf-8')
-        if senha.decode('utf-8')=='!':break;exit()
-        hashs=('sha224', 'sha256', 'sha3_512', 'sha1', 'sha3_384')
-        for i in hashs:
-            senha=eval(f"hashlib.{i}({senha})").hexdigest().encode('utf-8')
-        senha=senha.decode('utf-8')
-        try:pyperclip.copy(senha);print('hash moved to clipboard')
-        except:print('failed to copy your hash automatically')
-        print(senha)
-    except:pass
+import click
+external_input=False
+hashs=('sha224', 'sha256', 'sha3_512', 'sha1', 'sha3_384')
+@click.command()
+@click.option('--isenha', default=False, help='string to hash')
+@click.option('--ihash', default=False,help='hash method')
+def external(isenha, ihash):
+	global external_input
+	try:
+		if ',' in ihash:ihash=ihash.split(',')
+	except:pass
+	senha=''
+	if isenha:
+		external_input=True
+		if check(('all',False),ihash):ihash=hashs
+		if check((list,tuple),type(ihash)):
+			for i in ihash:
+				senha=eval(f"hashlib.{i}({senha.encode('utf-8')})").hexdigest()
+		else:senha=eval(f"hashlib.{ihash}({senha.encode('utf-8')})").hexdigest()
+		try:pyperclip.copy(senha);print('hash moved to clipboard')
+		except:print('failed to copy your hash automatically')
+		click.echo(senha)
+	if not external_input:
+		senha=''
+		while True:
+			try:
+				senha=str(input('> '))
+				if senha=='':new=random(100000, 999999);print(new);senha=str(new)
+				if senha=='!':break;exit()
+				for i in hashs:
+					senha=eval(f"hashlib.{i}({senha.encode('utf-8')})").hexdigest()
+				try:pyperclip.copy(senha);print('hash moved to clipboard')
+				except:print('failed to copy your hash automatically')
+				print(senha)
+			except:pass
+external()
