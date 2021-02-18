48db4f1d347c5627054de9ba9e0070b50656b1b1
DevAlek <78972830+DevAlek@users.noreply.github.com>
2021-02-18 12:07:05 -0300

    Update hasher.py

--- hasher.py
+++ hasher.py
@@ -2,10 +2,11 @@
 from ez import *
 import hashlib
 import os
-assure(('pyperclip','keyboard'), True)
+assure(('pyperclip'), True, True)
+import pyperclip
 while True:
-    if keyboard.is_pressed('escape'): exit()
     senha=str(input('> ')).encode('utf-8')
+    if senha.decode('utf-8')==!:exit()
     sha244=hashlib.sha224(senha).hexdigest().encode('utf-8')
     sha256=hashlib.sha256(sha244).hexdigest().encode('utf-8')
     sha3_512=hashlib.sha3_512(sha256).hexdigest().encode('utf-8')
